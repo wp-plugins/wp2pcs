2ee4cb9e1ea088505055f017ff3b34017c41bad3
frustigor <frustigor@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-11-14 02:06:38 +0000

    add image cache and some others
    git-svn-id: https://plugins.svn.wordpress.org/wp2pcs/trunk@804123 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-storage-image-outlink.php
+++ wp-storage-image-outlink.php
@@ -18,7 +18,7 @@ function wp_storage_print_image(){
 		}
 		if($outlink_uri == $outlink_perfix){
 			$outlink_type = get_option('wp_storage_to_pcs_outlink_type');
-			if(strpos($_SERVER['HTTP_REFERER'],home_url()) !== 0){
+			if(isset($_SERVER['HTTP_REFERER']) && !empty($_SERVER['HTTP_REFERER']) && strpos($_SERVER['HTTP_REFERER'],$home_url) !== 0){
 				header("Content-Type: text/html; charset=utf-8");
 				echo '防盗链！ ';
 				if($outlink_type == '200')echo '<a href="'.home_url($current_uri).'">原图</a> ';
@@ -30,6 +30,17 @@ function wp_storage_print_image(){
 			$image_path = $root_dir.str_replace('/'.$outlink_uri,'',$current_uri);
 			if(get_option('wp_to_pcs_app_key') == 'false')$outlink_type = '200';
 			if($outlink_type == '200'){
+				// 考虑到流量问题，必须增加缓存能力
+				date_default_timezone_set("PRC");// 把时间控制在中国
+				session_start(); 
+				header("Cache-Control: private, max-age=10800, pre-check=10800");
+				header("Pragma: private");
+				header("Expires: " . date(DATE_RFC822,strtotime(" 2 day")));
+				if(isset($_SERVER['HTTP_IF_MODIFIED_SINCE'])){
+				  header('Last-Modified: '.$_SERVER['HTTP_IF_MODIFIED_SINCE'],true,304);
+				  exit;
+				}
+				// 缓存结束
 				$pcs = new BaiduPCS($access_token);
 				//$result = $pcs->thumbnail($image_path,1600,3200,100);
 				$result = $pcs->downloadStream($image_path);
