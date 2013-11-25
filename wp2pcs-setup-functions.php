9cbd9de35ba108226eb551811cb28898de4427ab
frustigor <frustigor@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-11-25 13:23:33 +0000

    fix some bugs
    git-svn-id: https://plugins.svn.wordpress.org/wp2pcs/trunk@810113 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp2pcs-setup-functions.php
+++ wp2pcs-setup-functions.php
@@ -57,3 +57,41 @@ function wp_to_pcs_wp_current_request_url($query = array(),$remove = array()){
 	}
 	return $current_url;
 }
+
+// 判断文件或目录是否真的有可写权限
+// http://blog.csdn.net/liushuai_andy/article/details/8611433
+if ( ! function_exists('is_really_writable'))  
+{  
+    function is_really_writable($file)  
+    {  
+        // If we're on a Unix server with safe_mode off we call is_writable  
+        if (DIRECTORY_SEPARATOR == '/' AND @ini_get("safe_mode") == FALSE)  
+        {  
+            return is_writable($file);  
+        }  
+  
+        // For windows servers and safe_mode "on" installations we'll actually  
+        // write a file then read it.  Bah...  
+        if (is_dir($file))  
+        {  
+            $file = rtrim($file, '/').'/'.md5(mt_rand(1,100).mt_rand(1,100));  
+  
+            if (($fp = @fopen($file, FOPEN_WRITE_CREATE)) === FALSE)  
+            {  
+                return FALSE;  
+            }  
+  
+            fclose($fp);  
+            @chmod($file, DIR_WRITE_MODE);  
+            @unlink($file);  
+            return TRUE;  
+        }  
+        elseif ( ! is_file($file) OR ($fp = @fopen($file, FOPEN_WRITE_CREATE)) === FALSE)  
+        {  
+            return FALSE;  
+        }  
+  
+        fclose($fp);  
+        return TRUE;  
+    }  
+}
