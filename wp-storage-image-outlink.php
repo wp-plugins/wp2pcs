603a88b872aa0fbbefa6f0b0ca75c304b0851607
frustigor <frustigor@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-11-27 08:20:37 +0000

    update to 1.1.1
    git-svn-id: https://plugins.svn.wordpress.org/wp2pcs/tags/1.1.1@811227 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ wp-storage-image-outlink.php
@@ -0,0 +1,70 @@
+<?php
+
+// 创建一个函数，用来在wordpress中打印图片地址
+function wp2pcs_image_src($image_path){
+	// image_path是指相对于后台保存的存储目录的路径
+	// 例如 $file_path = /test/test.jpg
+	// 注意最前面加/
+	$outlink_perfix = trim(get_option('wp_storage_to_pcs_outlink_perfix'));
+	$image_src = '/'.$outlink_perfix.$image_path;
+	return home_url($image_src);
+}
+
+// 通过对URI的判断来获得图片远程信息
+add_action('init','wp_storage_print_image',-1);
+function wp_storage_print_image(){
+	if(is_admin())return;
+	$home_url = home_url();
+	$home_arr = array_filter(explode('/',$home_url));
+	$current_uri = urldecode($_SERVER["REQUEST_URI"]);
+	$uri_arr = array_values(array_filter(explode('/',$current_uri)));
+	$outlink_perfix = trim(get_option('wp_storage_to_pcs_outlink_perfix'));
+	$install_in_sub_dir = array_intersect($home_arr,$uri_arr); // 判断是否安装在子目录
+	if(!empty($uri_arr)){
+		if(empty($install_in_sub_dir)){
+			$outlink_uri = $uri_arr[0];
+		}else{
+			$outlink_uri = $uri_arr[count($install_in_sub_dir)];
+		}
+		if($outlink_uri == $outlink_perfix){
+			$outlink_type = get_option('wp_storage_to_pcs_outlink_type');
+			if(isset($_SERVER['HTTP_REFERER']) && !empty($_SERVER['HTTP_REFERER']) && strpos($_SERVER['HTTP_REFERER'],$home_url) !== 0){
+				header("Content-Type: text/html; charset=utf-8");
+				echo '防盗链！ ';
+				if($outlink_type == '200')echo '<a href="'.home_url($current_uri).'">原图</a> ';
+				echo '<a href="'.home_url().'">首页</a>';
+				exit;
+			}
+			$root_dir = get_option('wp_storage_to_pcs_root_dir');
+			$access_token = WP2PCS_APP_TOKEN;
+			$image_path = $root_dir.str_replace_first_time('/'.$outlink_uri.'/','/',$current_uri);
+			$image_path = str_replace('//','/',$image_path);
+			if($outlink_type == '200'){
+				// 考虑到流量问题，必须增加缓存能力
+				date_default_timezone_set("PRC");// 把时间控制在中国
+				session_start(); 
+				header("Cache-Control: private, max-age=10800, pre-check=10800");
+				header("Pragma: private");
+				header("Expires: " . date(DATE_RFC822,strtotime(" 2 day")));
+				if(isset($_SERVER['HTTP_IF_MODIFIED_SINCE'])){
+				  header('Last-Modified: '.$_SERVER['HTTP_IF_MODIFIED_SINCE'],true,304);
+				  exit;
+				}
+				// 缓存结束
+				$pcs = new BaiduPCS($access_token);
+				//$result = $pcs->thumbnail($image_path,1600,3200,100);
+				$result = $pcs->downloadStream($image_path);
+				ob_clean();
+				header('Content-type: image/jpeg');
+				echo $result;
+			}else{
+				//$image_outlink = 'https://pcs.baidu.com/rest/2.0/pcs/thumbnail?method=generate&access_token='.$access_token.'&path='.$image_path.'&quality=100&width=1600&height=1600';
+				$site_id = get_option('wp_to_pcs_site_id');
+				$access_token = substr($access_token,0,10);
+				$image_outlink = 'http://wp2pcs.duapp.com/img?'.$site_id.'+'.$access_token.'+path='.$image_path;
+				header('Location:'.$image_outlink);
+			}
+			exit;
+		}
+	}
+}
---
+++ wp2pcs-setup-functions.php
@@ -0,0 +1,109 @@
+<?php
+
+// 创建一个函数，用来获取当前PHP的执行时间
+function get_unix_timestamp(){   
+    list($msec,$sec) = explode(' ',microtime());
+    return (float)$sec+(float)$msec;
+}
+// 利用上面的函数，获取php开始执行的时间戳。注意，这是一个全局函数
+$php_begin_run_time = get_unix_timestamp();
+
+// 创建一个函数，获取php执行了的时间，以秒为单位（浮点数）
+function get_php_run_time(){
+	global $php_begin_run_time;
+	$php_run_time = get_unix_timestamp() - $php_begin_run_time;
+	return $php_run_time;
+}
+
+// 创建一个函数，判断插件是否已经激活
+function is_wp_to_pcs_active(){
+	$app_key = get_option('wp_to_pcs_app_key');
+	$access_token = get_option('wp_to_pcs_access_token');
+	if(!$app_key || !$access_token){
+		return false;
+	}
+	return true;
+}
+
+// 获取当前访问的URL地址
+function wp_to_pcs_wp_current_request_url($query = array(),$remove = array()){
+	// http://www.ludou.org/how-to-get-the-current-url-in-wordpress.html
+	global $wp;
+	$home_root = substr_count(home_url(),'/') <= 3 ? true : false;
+	$permalink = trim(get_option('permalink_structure')) != '' ? true : false;
+	if($home_root){
+		$current_url = home_url(add_query_arg(array()));
+	}
+	if(!$home_root && $permalink){
+		$current_url = home_url(add_query_arg(array(),$wp->request));
+	}
+	if(!$home_root && !$permalink){
+		$current_url = add_query_arg($wp->query_string,'',home_url($wp->request));
+	}
+	if($current_url == home_url()){
+		$current_url = home_url('/');
+	}
+	$parse_url = parse_url($current_url);
+	if(is_array($query) && !empty($query)){
+		parse_str($parse_url['query'],$parse_query);
+		$parse_query = array_merge($parse_query,$query);
+		if(!empty($remove))foreach($remove as $key){
+			if(isset($parse_query[$key]))unset($parse_query[$key]);
+		}
+		$parse_query = http_build_query($parse_query);
+		$current_url = str_replace($parse_url['query'],'?'.$parse_query,$current_url);
+	}elseif($query === false){
+		$current_url = str_replace('?'.$parse_url['query'],'',$current_url);
+	}
+	return $current_url;
+}
+
+// 判断文件或目录是否真的有可写权限
+// http://blog.csdn.net/liushuai_andy/article/details/8611433
+if ( ! function_exists('is_really_writable'))  
+{  
+    function is_really_writable($file)  
+    {  
+        // If we're on a Unix server with safe_mode off we call is_writable  
+        if (DIRECTORY_SEPARATOR == '/' AND @ini_get("safe_mode") == FALSE)  
+        {  
+            return is_writable($file);  
+        }  
+  
+        // For windows servers and safe_mode "on" installations we'll actually  
+        // write a file then read it.  Bah...  
+        if (is_dir($file))  
+        {  
+            $file = rtrim($file, '/').'/'.md5(mt_rand(1,100).mt_rand(1,100));  
+  
+            if (($fp = @fopen($file, FOPEN_WRITE_CREATE)) === FALSE)  
+            {  
+                return FALSE;  
+            }  
+  
+            fclose($fp);  
+            @chmod($file, DIR_WRITE_MODE);  
+            @unlink($file);  
+            return TRUE;  
+        }  
+        elseif ( ! is_file($file) OR ($fp = @fopen($file, FOPEN_WRITE_CREATE)) === FALSE)  
+        {  
+            return FALSE;  
+        }  
+  
+        fclose($fp);  
+        return TRUE;  
+    }  
+}
+
+// 替换字符串中第一次出现的子串
+function str_replace_first_time($find,$replace,$string){
+	$position = strpos($string,$find);
+	if($position !== false){
+		$length = strlen($find); 
+		$string = substr_replace($string,$replace,$position,$length);
+		return $string;
+	}else{
+		return false;
+	}
+}
