93b5ece95484afbea3aa13feb21cedef6c8d23bc
frustigor <frustigor@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-11-30 14:23:23 +0000

    fix some bugs
    git-svn-id: https://plugins.svn.wordpress.org/wp2pcs/tags/1.1.2@812863 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-storage-download-file.php
+++ wp-storage-download-file.php
@@ -83,7 +83,7 @@ function wp_storage_download_file(){
 		echo $result;
 	}else{
 		$site_id = get_option('wp_to_pcs_site_id');
-		$access_token = substr($access_token,0,10);
+		$access_token = substr(WP2PCS_APP_TOKEN,0,10);
 		$download_link = 'http://wp2pcs.duapp.com/dl?'.$site_id.'+'.$access_token.'+path='.$file_path;
 		header('Location:'.$download_link);
 	}
