d3d44525299a312d481a5a62950741e2c81b6e12
frustigor <frustigor@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-12 13:17:16 +0000

    去除了两个多余的标签（可能导致页面错乱）
    git-svn-id: https://plugins.svn.wordpress.org/wp2pcs/trunk@1043440 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin/about.php
+++ admin/about.php
@@ -8,13 +8,12 @@
   <a href="javascript:void(0);" class="nav-tab nav-tab-active">关于</a>
 </h2>
 
-<div class="metabox-holder"><div class="meta-box-sortables">
-
 <div class="changelog point-releases">
   <h3>全新的1.4.0版本，为你带来超简化的酷爽体验！</h3>
   <p>WP2PCS 1.4.0版是一个全新的版本，摒弃了以前所有版本，重构代码，实现性能和使用的体验提升。</p>
   <?php if(get_option('wp_to_pcs_app_token')) { echo '<p style="color: #DA4F25;">检测到你是从低版本升级上来的，1.4.0版本是一个全新的版本，以前的所有设置都无效了，你需要花点时间来看下怎么升级。点击<a href="http://www.wp2pcs.com/?p=432" target="_blank">这里</a>查看</p>'; } ?>
 </div>
+
 <div class="changelog">
   <div class="about-overview"><img class="about-overview-img" src="<?php echo plugins_url('assets/about.png',WP2PCS_PLUGIN_NAME); ?>" width="640" height="360" /></div>
   <hr />
@@ -39,6 +38,7 @@
   </div>
   <hr>
 </div>
+
 <div class="changelog under-the-hood">
   <h3>核心功能</h3>
   <div class="feature-section col three-col">
