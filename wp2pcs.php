5dd8fd94cf8440598b040a58db576e861e159aa2
frustigor <frustigor@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-12 13:01:30 +0000

    去除其中关于谷歌被墙的代码，以防跟其他类抵触
    git-svn-id: https://plugins.svn.wordpress.org/wp2pcs/trunk@1043423 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp2pcs.php
+++ wp2pcs.php
@@ -61,20 +61,6 @@ class WP2PCS {
 }
 $WP2PCS = new WP2PCS;
 
-// 谷歌被墙，后台字体加载慢，禁用
-class Disable_Google_Fonts {
-  public function __construct() {
-    add_filter( 'gettext_with_context', array( $this, 'disable_open_sans' ), 888, 4 );
-  }
-  public function disable_open_sans( $translations, $text, $context, $domain ) {
-    if ( 'Open Sans font: on or off' == $context && 'on' == $text ) {
-      $translations = 'off';
-    }
-    return $translations;
-  }
-}
-$disable_google_fonts = new Disable_Google_Fonts();
-
 $function_files_path = dirname(WP2PCS_PLUGIN_NAME).'/hook';
 if(file_exists($function_files_path)):
 $function_files = scandir($function_files_path);
