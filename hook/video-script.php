62beadcb5282501ae5ece9e85b3a83bfefd1972c
frustigor <frustigor@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-15 09:57:22 +0000

    改进视频播放器脚本，不用再跑到百度服务器去抓取，而是直接在本地使用脚本输出，更加安全，加载也更快，基于此，把其缓存也删除了
    git-svn-id: https://plugins.svn.wordpress.org/wp2pcs/trunk@1044816 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- hook/video-script.php
+++ hook/video-script.php
@@ -66,52 +66,28 @@ else {
 
 // 显示播放器
 if(isset($_GET['path']) && !empty($_GET['path']) && isset($_GET['md5']) && !empty($_GET['md5']) && isset($_GET['video']) && !empty($_GET['video'])) {
-  // 因为没有加载WordPress，所以需要自己创建函数
-  if(!function_exists('get_by_curl')) :
-  function get_by_curl($url,$post = false,$referer = false){
-    $ch = curl_init();
-    curl_setopt($ch,CURLOPT_URL,$url);
-    curl_setopt($ch, CURLOPT_HEADER, 0);
-    if($referer) {
-      curl_setopt ($ch,CURLOPT_REFERER,$referer);
-    }
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_FAILONERROR, false);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-    if($post){
-      curl_setopt($ch, CURLOPT_POST, 1);
-      curl_setopt($ch, CURLOPT_POSTFIELDS,$post);
-    }
-    $result = curl_exec($ch);
-    curl_close($ch);
-    return $result;
-  }
-  endif;
-  // 显示播放器的内容
-  $host = $_SERVER["HTTP_HOST"];
-  $host = strpos($host,':') === false ? $host : substr($host,0,strpos($host,':'));
-  $referer = isset($_SERVER["HTTP_REFERER"]) && !empty($_SERVER["HTTP_REFERER"]) ? $_SERVER["HTTP_REFERER"] : null;
-  if(!$referer || strpos($_SERVER["HTTP_REFERER"],$host) === false) {
-    exit;
-  }
-  // 缓存
-  header("Cache-Control: private, max-age=10800, pre-check=10800");
-  header("Pragma: private");
-  header("Expires: " . date(DATE_RFC822,strtotime(" 2 day")));
-  if(isset($_SERVER['HTTP_IF_MODIFIED_SINCE'])){
-    header('Last-Modified: '.$_SERVER['HTTP_IF_MODIFIED_SINCE'],true,304);
-    exit;
-  }
-  // 抓取显示
-  $path = $_GET['path'];
-  $md5 = $_GET['md5'];
-  $url = 'https://pan.baidu.com/res/static/thirdparty/guanjia/guanjia_play.html?path='.$path.'&md5='.$md5;
-  $html = get_by_curl($url,false,$url);
-  $html = str_replace('</title>','</title><base href="https://pan.baidu.com/" />',$html);
-  $html = str_replace('</body>','',$html);
-  $html = str_replace('</html>','',$html);
-  echo $html;
 ?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta charset="utf-8"/>
+<title>WP2PCS视频播放</title>
+<base href="http://pan.baidu.com"/>
+<link href="/res/static/thirdparty/guanjia/css/guanjia_video_all.css?t=201412054621" rel="stylesheet"/>
+</head>
+<body>
+<?php
+// 判断来路，如果不是当前网站，不显示任何内容
+$host = $_SERVER["HTTP_HOST"];
+$host = strpos($host,':') === false ? $host : substr($host,0,strpos($host,':'));
+$referer = isset($_SERVER["HTTP_REFERER"]) && !empty($_SERVER["HTTP_REFERER"]) ? $_SERVER["HTTP_REFERER"] : null;
+if($referer && strpos($_SERVER["HTTP_REFERER"],$host) !== false) {
+?>
+<div class="guanjia_panl" id="guanjia_panl">
+</div>
+<script src="/res/static/thirdparty/guanjia/js/guanjia_video_all.js?t=201412054621" type="text/javascript"></script>
+<script src="/res/static/thirdparty/flashvideo/js/cyberplayer.min.js" type="text/javascript"></script>
+<script type="text/javascript">/*<![CDATA[*/$(document).ready(function(){var D=function(){disk.ui.VideoFlash.prototype.getVideoPath=function(){return"/api/streaming?path="+disk.getParam("path")+"&type=M3U8_AUTO_480";};C=decodeURIComponent(C);var A=disk.ui.VideoFlash.obtain(),_={path:C,target:"guanjia_panl",type:2,md5:E,isGuanJia:true,onSeek:function(){},onTime:function(){}};A.play(_);disk.ui.VideoFlash.getStorageItem(E,function(_){if(disk.ui.VideoFlash.flashPlayer){disk.ui.VideoFlash.flashPlayer.seek(_);}});disk.ui.GuanJiaVideo.installFuncTips();if(disk.DEBUG){}},C=disk.getParam("path"),E=disk.getParam("md5")||"",B=$("#guanjia_panl"),_=function(){B.html('<div class="nofile">\u6587\u4ef6\u52a0\u8f7d\u5931\u8d25</div>');};if(!C){_();return;}if(parseInt(disk.getParam("safebox"),10)===1){D();$.get("/api/streaming?path="+disk.getParam("path")+"&type=M3U8_AUTO_480",function(_){try{_=$.parseJSON(_);if(_.errno===27){try{BDHScript.throwEvent("LockSafebox",{lock:1});}catch(A){}}else{}}catch(A){}});}else{C=decodeURIComponent(C);var F=disk.ui.VideoFlash.obtain(),A={path:C,target:"guanjia_panl",type:2,md5:E,isGuanJia:true,onSeek:function(){},onTime:function(){}};F.play(A);disk.ui.VideoFlash.getStorageItem(E,function(_){if(disk.ui.VideoFlash.flashPlayer){disk.ui.VideoFlash.flashPlayer.seek(_);}});disk.ui.GuanJiaVideo.installFuncTips();if(disk.DEBUG){}}});/*]]>*/</script>
 <script>
 jQuery(function($){
   $('.video-functions-tips').remove();
@@ -120,6 +96,9 @@ jQuery(function($){
   });
 });
 </script>
+<?php
+}
+?>
 </body>
 </html>
 <?php
