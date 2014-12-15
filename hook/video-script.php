05b82429d083796e7649bd5778f8a47307e2ed6c
frustigor <frustigor@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-15 08:30:04 +0000

    因为之前移动了代码，所以hook中的代码其实应该做相应的调整
    git-svn-id: https://plugins.svn.wordpress.org/wp2pcs/trunk@1044775 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- hook/video-script.php
+++ hook/video-script.php
@@ -1,63 +1,6 @@
 <?php
 
-if(!function_exists('get_by_curl')) :
-function get_by_curl($url,$post = false,$referer = false){
-  $ch = curl_init();
-  curl_setopt($ch,CURLOPT_URL,$url);
-  curl_setopt($ch, CURLOPT_HEADER, 0);
-  if($referer) {
-    curl_setopt ($ch,CURLOPT_REFERER,$referer);
-  }
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-  if($post){
-    curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_POSTFIELDS,$post);
-  }
-  $result = curl_exec($ch);
-  curl_close($ch);
-  return $result;
-}
-endif;
-
-// 显示播放器
-if(isset($_GET['path']) && !empty($_GET['path']) && isset($_GET['md5']) && !empty($_GET['md5']) && isset($_GET['video']) && !empty($_GET['video'])) {
-  $host = $_SERVER["HTTP_HOST"];
-  $host = strpos($host,':') === false ? $host : substr($host,0,strpos($host,':'));
-  $referer = isset($_SERVER["HTTP_REFERER"]) && !empty($_SERVER["HTTP_REFERER"]) ? $_SERVER["HTTP_REFERER"] : null;
-  if(!$referer || strpos($_SERVER["HTTP_REFERER"],$host) === false) {
-    exit;
-  }
-  header("Cache-Control: private, max-age=10800, pre-check=10800");
-  header("Pragma: private");
-  header("Expires: " . date(DATE_RFC822,strtotime(" 2 day")));
-  if(isset($_SERVER['HTTP_IF_MODIFIED_SINCE'])){
-    header('Last-Modified: '.$_SERVER['HTTP_IF_MODIFIED_SINCE'],true,304);
-    exit;
-  }
-
-  $path = $_GET['path'];
-  $md5 = $_GET['md5'];
-  $url = 'http://pan.baidu.com/res/static/thirdparty/guanjia/guanjia_play.html?path='.$path.'&md5='.$md5;
-  $html = get_by_curl($url,false,$url);
-  $html = str_replace('</title>','</title><base href="http://pan.baidu.com/" />',$html);
-  $html = str_replace('</body>','',$html);
-  $html = str_replace('</html>','',$html);
-  echo $html;
-?>
-<script>
-jQuery(function($){
-  $('.video-functions-tips').remove();
-  $(document).bind("contextmenu",function(e){   
-    return false;   
-  });
-});
-</script>
-</body>
-</html>
-<?php
-  exit();
-}
-
+// 基本的视频播放器样式css，因为两种情况下都要用，所以放在最前面
 function wp2pcs_video_player_css_in_admin_editor() {
 ?>
 .wp2pcs-video-player {display:block;width:480px;height:360px;margin: 1em auto;}
@@ -71,14 +14,6 @@ function wp2pcs_video_player_css_in_admin_editor() {
 }
 .wp2pcs-video-player iframe {display:block;width:100%;height:100%;}
 <?php
-}
-
-// 直接访问文件的时候打印
-if(isset($_GET['script']) && $_GET['script'] == 'style.css') {
-  header('Content-Type: text/css; charset=utf-8');
-  wp2pcs_video_player_css_in_admin_editor();
-  echo '.wp2pcs-video-player a {background-image:url(../assets/video-play.png);}';
-  exit;
 }
 
 // 判断如果加载了WordPress
@@ -118,10 +53,86 @@ jQuery(function($){
 <?php
 }
 
-// 加入到后台编辑器中
-add_action( 'init','wp2pcs_video_player_css_in_admin_editor_link');
-function wp2pcs_video_player_css_in_admin_editor_link() {
-  add_editor_style(plugins_url('hook/video-script.php?script=style.css',WP2PCS_PLUGIN_NAME));
+// 加入到后台编辑器中css
+add_editor_style(plugins_url('hook/video-script.php?script=style.css',WP2PCS_PLUGIN_NAME));
+
+}
+// 如果没有加载WordPress的话
+else {
+
+
+// 显示播放器
+if(isset($_GET['path']) && !empty($_GET['path']) && isset($_GET['md5']) && !empty($_GET['md5']) && isset($_GET['video']) && !empty($_GET['video'])) {
+  // 因为没有加载WordPress，所以需要自己创建函数
+  if(!function_exists('get_by_curl')) :
+  function get_by_curl($url,$post = false,$referer = false){
+    $ch = curl_init();
+    curl_setopt($ch,CURLOPT_URL,$url);
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    if($referer) {
+      curl_setopt ($ch,CURLOPT_REFERER,$referer);
+    }
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    if($post){
+      curl_setopt($ch, CURLOPT_POST, 1);
+      curl_setopt($ch, CURLOPT_POSTFIELDS,$post);
+    }
+    $result = curl_exec($ch);
+    curl_close($ch);
+    return $result;
+  }
+  endif;
+  // 显示播放器的内容
+  $host = $_SERVER["HTTP_HOST"];
+  $host = strpos($host,':') === false ? $host : substr($host,0,strpos($host,':'));
+  $referer = isset($_SERVER["HTTP_REFERER"]) && !empty($_SERVER["HTTP_REFERER"]) ? $_SERVER["HTTP_REFERER"] : null;
+  if(!$referer || strpos($_SERVER["HTTP_REFERER"],$host) === false) {
+    exit;
+  }
+  header("Cache-Control: private, max-age=10800, pre-check=10800");
+  header("Pragma: private");
+  header("Expires: " . date(DATE_RFC822,strtotime(" 2 day")));
+  if(isset($_SERVER['HTTP_IF_MODIFIED_SINCE'])){
+    header('Last-Modified: '.$_SERVER['HTTP_IF_MODIFIED_SINCE'],true,304);
+    exit;
+  }
+
+  $path = $_GET['path'];
+  $md5 = $_GET['md5'];
+  $url = 'https://pan.baidu.com/res/static/thirdparty/guanjia/guanjia_play.html?path='.$path.'&md5='.$md5;
+  $html = get_by_curl($url,false,$url);
+  $html = str_replace('</title>','</title><base href="http://pan.baidu.com/" />',$html);
+  $html = str_replace('</body>','',$html);
+  $html = str_replace('</html>','',$html);
+  echo $html;
+?>
+<script>
+jQuery(function($){
+  $('.video-functions-tips').remove();
+  $(document).bind("contextmenu",function(e){   
+    return false;   
+  });
+});
+</script>
+</body>
+</html>
+<?php
+  exit();
 }
 
-} // endif
+// 直接访问文件的时候打印CSS
+if(isset($_GET['script']) && $_GET['script'] == 'style.css') {
+  header("Cache-Control: private, max-age=10800, pre-check=10800");
+  header("Pragma: private");
+  header("Expires: " . date(DATE_RFC822,strtotime(" 2 day")));
+  if(isset($_SERVER['HTTP_IF_MODIFIED_SINCE'])){
+    header('Last-Modified: '.$_SERVER['HTTP_IF_MODIFIED_SINCE'],true,304);
+    exit;
+  }
+  header('Content-Type: text/css; charset=utf-8');
+  wp2pcs_video_player_css_in_admin_editor();
+  echo '.wp2pcs-video-player a {background-image:url(../assets/video-play.png);}';
+  exit;
+}
+
+}
