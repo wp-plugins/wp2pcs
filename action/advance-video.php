15ea2ba6c429aa4d1b32482aac03dbe18fe65859
frustigor <frustigor@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-17 08:08:26 +0000

    关闭m3u8时同时关闭插入视频播放器
    git-svn-id: https://plugins.svn.wordpress.org/wp2pcs/trunk@1092229 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- action/advance-video.php
+++ action/advance-video.php
@@ -3,6 +3,11 @@
 if(isset($_POST['action']) && $_POST['action'] == 'update-video-setting') {
   check_admin_referer();
   update_option('wp2pcs_video_m3u8',$_POST['wp2pcs_video_m3u8']);
-  update_option('wp2pcs_load_videoplay',$_POST['wp2pcs_load_videoplay']);
+  if($_POST['wp2pcs_video_m3u8']) {
+    update_option('wp2pcs_load_videoplay',$_POST['wp2pcs_load_videoplay']);
+  }
+  else {
+    update_option('wp2pcs_load_videoplay',0);
+  }
   wp_redirect(add_query_arg(array('tab'=>'video','time'=>time()),menu_page_url('wp2pcs-advance',false)));
 }
