62f0175f0c48da66b23173cac0269768a8018f67
frustigor <frustigor@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-21 15:00:29 +0000

    修改关于页面的显示跳转，改为javascript，top.location，防止后台自动升级时无法看到
    git-svn-id: https://plugins.svn.wordpress.org/wp2pcs/trunk@1095899 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- hook/install.php
+++ hook/install.php
@@ -10,6 +10,7 @@ function wp2pcs_install_redirect() {
   $plugin_version = get_option('wp2pcs_plugin_version');
   if($plugin_version < WP2PCS_PLUGIN_VERSION) {
     update_option('wp2pcs_plugin_version',WP2PCS_PLUGIN_VERSION);
-    wp_redirect(add_query_arg(array('tab'=>'about','time'=>time()),menu_page_url('wp2pcs-setting',false)));
+    echo '<script>top.location.href="'.add_query_arg(array('tab'=>'about','time'=>time()),menu_page_url('wp2pcs-setting',false)).'";</script>';
+    exit();
   }
 }
