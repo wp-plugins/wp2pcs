d90ceee6beb568ac059ab2f7b278985985f0dc9f
frustigor <frustigor@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-28 13:53:57 +0000

    修复如果过期的情况下，m3u8和player本来是开启的时候的情况
    git-svn-id: https://plugins.svn.wordpress.org/wp2pcs/trunk@1101857 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin/advance-video.php
+++ admin/advance-video.php
@@ -28,13 +28,13 @@ $wp2pcs_video_player = (int)get_option('wp2pcs_video_player');
   <div class="inside">
     <p>开启m3u8视频服务？<select name="wp2pcs_video_m3u8">
         <option value="0" <?php selected($wp2pcs_video_m3u8,0); ?>>关闭</option>
-        <option value="1" <?php selected($wp2pcs_video_m3u8,1);if(!$wp2pcs_site_id || time() > $wp2pcs_site_expire) echo ' disabled'; ?>>开启</option>
+        <option value="1" <?php if($wp2pcs_site_id && time() < $wp2pcs_site_expire) selected($wp2pcs_video_m3u8,1); else echo ' disabled'; ?>>开启</option>
       </select>
     </p>
     <p><small>关闭后，之前使用m3u8播放的视频将采用免费模式播放。开启后，自动启用“插入视频时插入视频播放器”功能。</small></p>
     <p>插入视频时插入视频播放器？<select name="wp2pcs_video_player">
         <option value="0" <?php selected($wp2pcs_video_player,0); ?>>关闭</option>
-        <option value="1" <?php selected($wp2pcs_video_player,1);if(!$wp2pcs_site_id || time() > $wp2pcs_site_expire) echo ' disabled'; ?>>开启</option>
+        <option value="1" <?php if($wp2pcs_site_id && time() < $wp2pcs_site_expire) selected($wp2pcs_video_player,1); else echo ' disabled'; ?>>开启</option>
       </select>
       <small>免费版的视频播放功能已经不能使用了</small>
     </p>
