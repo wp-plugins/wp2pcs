785f639fbb9d460e50555029ed786e474f677b6b
frustigor <frustigor@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-27 02:07:45 +0000

    去除多余选项
    git-svn-id: https://plugins.svn.wordpress.org/wp2pcs/trunk@1121883 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin/advance-outlink.php
+++ admin/advance-outlink.php
@@ -4,7 +4,7 @@
   <a href="<?php menu_page_url('wp2pcs-advance'); ?>" class="nav-tab">站点</a>
   <a href="javascript:void(0)" class="nav-tab nav-tab-active">外链</a>
   <a href="<?php echo add_query_arg('tab','video',menu_page_url('wp2pcs-advance',false)); ?>" class="nav-tab">视频</a>
-  <a href="<?php echo add_query_arg('tab','image',menu_page_url('wp2pcs-advance',false)); ?>" class="nav-tab">图片</a>
+  <small style="font-size:12px;float:right;">付费用户如遇问题请发邮件到<code>476206120@qq.com</code>。不支持qq（群）提问。</small>
 </h2>
 
 <div class="metabox-holder"><div class="meta-box-sortables">
